024dd88b34792cf117bfda62d0cf5d25471d9eaa
Dan Fox <dhf2001@gmail.com>
2022-02-09 15:11:01 -0500

    Write basic code to render a 2D particle

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
+use svg::node::element::Circle;
 use svg::Document;
-use svg::node::element::Path;
-use svg::node::element::path::Data;
+
+struct Particle {
+    x: f32,
+    y: f32,
+    radius: f32,
+}
 
 fn main() {
-    let data = Data::new()
-        .move_to((10, 10))
-        .line_by((0, 50))
-        .line_by((50, 0))
-        .line_by((0, -50))
-        .close();
+    let particle = Particle {
+        x: 0.0,
+        y: 0.0,
+        radius: 10.0,
+    };
 
-    let path = Path::new()
-        .set("fill", "none")
-        .set("stroke", "black")
-        .set("stroke-width", 3)
-        .set("d", data);
+    save_svg(particle);
+}
+
+fn save_svg(particle: Particle) {
+    let circle = Circle::new()
+        .set("fill", "black")
+        .set("cx", particle.x)
+        .set("cy", particle.y)
+        .set("r", particle.radius);
 
     let document = Document::new()
-        .set("viewBox", (0, 0, 70, 70))
-        .add(path);
+        .set("viewBox", (-50, -50, 100, 100))
+        .add(circle);
 
     svg::save("data/image.svg", &document).unwrap();
 }
