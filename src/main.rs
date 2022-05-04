07e88612b12cd2e1157872e567978a2b26288dd2
Dan Fox <dhf2001@gmail.com>
2022-05-03 21:21:04 -0400

    Create a stub for the DLA

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,51 @@
 use svg::node::element::Circle;
 use svg::Document;
 
-struct Particle {
-    x: f32,
-    y: f32,
-    radius: f32,
+pub struct Particle {
+    pub x: f32,
+    pub y: f32,
+    pub radius: f32,
+}
+
+pub struct DLA {
+    pub particles: Vec<Particle>,
+}
+
+impl DLA {
+    pub fn new(seed_particles: Vec<Particle>) -> DLA {
+        DLA {
+            particles: seed_particles,
+        }
+    }
 }
 
 fn main() {
-    let particle = Particle {
+    let seed_particles = vec![Particle {
         x: 0.0,
         y: 0.0,
         radius: 10.0,
-    };
+    }];
 
-    save_svg(particle);
+    let dla = DLA::new(seed_particles);
+
+    save_svg(dla);
 }
 
-fn save_svg(particle: Particle) {
-    let circle = Circle::new()
+fn save_svg(dla: DLA) {
+    let mut document = Document::new().set("viewBox", (-50, -50, 100, 100));
+
+    for particle in &dla.particles {
+        // TODO: fix clone (work around #add consuming self issue)
+        document = document.clone().add(circle(particle));
+    }
+
+    svg::save("data/image.svg", &document).unwrap();
+}
+
+fn circle(particle: &Particle) -> Circle {
+    Circle::new()
         .set("fill", "black")
         .set("cx", particle.x)
         .set("cy", particle.y)
-        .set("r", particle.radius);
-
-    let document = Document::new()
-        .set("viewBox", (-50, -50, 100, 100))
-        .add(circle);
-
-    svg::save("data/image.svg", &document).unwrap();
+        .set("r", particle.radius)
 }
